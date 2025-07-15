0124949d5d3b2280acf8fdfe68a709642034bcde
ZanRoberto <zroberto59@gmail.com>
2025-07-15 12:01:27 +0200

    Update bridge_scraper.py

--- bridge_scraper.py
+++ bridge_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import re
 
 def estrai_testo_vocami():
-    url = "https://www.vocami.it/mirami/?qr=63AE29D71905EF509A564E58E576D123"
+    url = "https://docs.google.com/document/d/e/2PACX-1vSqy0-FZAqOGvnCFZwwuBfT1cwXFpmSpkWfrRiT8RlbQpdQy-_1hOaqIslih5ULSa0XhVt0V8QeWJDP/pub"
     headers = {"User-Agent": "Mozilla/5.0"}
     try:
         response = requests.get(url, headers=headers, timeout=10)
